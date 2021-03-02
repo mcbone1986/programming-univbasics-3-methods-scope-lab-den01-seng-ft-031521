60edd450addbc02b33250cb9ab3becdda1c2690b
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 01:18:06 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,24 @@
+def link 
+  say = "It's Dangerous To Go Alone! Take This."
+  puts say
+end
+#link 
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+#mario 
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+#toadstool
+
+def all_phrases
+  puts link
+  puts mario 
+  puts toadstool
+end
+all_phrases
