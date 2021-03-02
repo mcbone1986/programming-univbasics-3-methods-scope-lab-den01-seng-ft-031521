ad8b2964a6e453f66631d535cb1ff6f5313306b5
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 01:17:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,16 +2,23 @@ def link
   say = "It's Dangerous To Go Alone! Take This."
   puts say
 end
-link 
+#link 
 
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-mario 
+#mario 
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-toadstool
+#toadstool
+
+def all_phrases
+  puts link
+  puts mario 
+  puts toadstool
+end
+all_phrases
