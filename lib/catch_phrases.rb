8fe77daaa454a3bb97c8226a9eb02363ca725f2f
Matthew Bonebrake <matthewcbonebrake@gmail.com>
2021-03-02 01:15:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,13 +3,16 @@ def link
   puts say
   say
 end
+link 
 
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
+mario 
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+toadstool
